a18c15dd4e5ad521cae2947c324a63460b37de70
aquapaka <aqua.tamlong@gmail.com>
2025-03-04 16:09:08 +0700

    feat: configure react native paper

--- App.tsx
+++ App.tsx
@@ -1,10 +1,37 @@
-import { NavigationContainer } from "@react-navigation/native";
+import "@expo/metro-runtime";
 import RootTabNavigator from "./src/navigation/RootTabNavigator";
+import { PaperProvider } from "react-native-paper";
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from "@react-navigation/native";
+import {
+  MD3DarkTheme,
+  MD3LightTheme,
+  adaptNavigationTheme,
+} from "react-native-paper";
+import merge from "deepmerge";
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const combinedDefaultTheme = merge(MD3LightTheme, LightTheme);
+const combinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootTabNavigator />
-    </NavigationContainer>
+    <PaperProvider theme={combinedDarkTheme}>
+      <NavigationContainer
+        theme={{
+          ...combinedDarkTheme,
+          fonts: { ...NavigationDarkTheme.fonts },
+        }}
+      >
+        <RootTabNavigator />
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- src/screens/favorite/index.tsx
+++ src/screens/favorite/index.tsx
@@ -1,3 +1,36 @@
+import { useState } from "react";
+import { View } from "react-native";
+import { Menu, Divider, Button } from "react-native-paper";
+
 export default function FavoriteScreen() {
-  return <></>;
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+
+  return (
+    <View
+      style={{
+        paddingTop: 50,
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+    >
+      <Menu
+        visible={visible}
+        onDismiss={closeMenu}
+        anchor={<Button onPress={openMenu}>Show menu</Button>}
+      >
+        <Menu.Item
+          leadingIcon={"lock-alert-outline"}
+          onPress={() => {}}
+          title="Item 1"
+        />
+        <Menu.Item leadingIcon={"redo"} onPress={() => {}} title="Item 2" />
+        <Divider />
+        <Menu.Item onPress={() => {}} title="Item 3" />
+      </Menu>
+    </View>
+  );
 }
