cc75b88b5c820993f0dbbfaedfea1a2a99a798f8
aquapaka <aqua.tamlong@gmail.com>
2025-03-05 11:39:00 +0700

    feat: filter by brand

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,11 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { FlatList, View } from "react-native";
 import artToolApi from "../../apis/artToolApi";
 import ArtToolCard from "../../components/ArtToolCard";
 import { ArtTool } from "../../types/artTool";
+import { Chip } from "react-native-paper";
 
 export default function HomeScreen() {
   const [artTools, setArtTools] = useState<ArtTool[]>([]);
+  const brands = useMemo(
+    () => [...new Set(artTools.map((artTool) => artTool.brand))],
+    [artTools]
+  );
+  const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
+  const filteredArtTools = useMemo(
+    () => artTools.filter((artTool) => selectedBrands.includes(artTool.brand)),
+    [artTools, selectedBrands]
+  );
 
   useEffect(() => {
     async function fetchApi() {
@@ -19,11 +29,30 @@ export default function HomeScreen() {
   return (
     <View style={{ padding: 8 }}>
       <FlatList
-        data={artTools}
+        ListHeaderComponent={
+          <View style={{ flexDirection: "row", gap: 8, flexWrap: "wrap" }}>
+            {brands.map((brand) => (
+              <Chip
+                selected={selectedBrands.includes(brand)}
+                onPress={() =>
+                  setSelectedBrands((selectedBrands) =>
+                    selectedBrands.includes(brand)
+                      ? selectedBrands.filter((b) => b !== brand)
+                      : [brand, ...selectedBrands]
+                  )
+                }
+              >
+                {brand}
+              </Chip>
+            ))}
+          </View>
+        }
+        data={filteredArtTools.length ? filteredArtTools : artTools}
         keyExtractor={(item) => item.id}
         numColumns={2}
         contentContainerStyle={{
           alignItems: "stretch",
+          gap: 8,
         }}
         columnWrapperStyle={{
           justifyContent: "space-between",
